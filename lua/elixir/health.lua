fefbf7d0bf5e8fbf49d1715256a27255e435e2fa
Ndrianiaina Mandrantosoa <tosoa@tag-ip.com>
2023-05-15 15:28:16 +0300

    fix: vim.health.start is nil

--- lua/elixir/health.lua
+++ lua/elixir/health.lua
@@ -2,22 +2,22 @@ local V = require("elixir.elixirls.version")
 local M = {}
 
 function M.check()
-  vim.health.start("elixir-tools.nvim report")
+  vim.health.report_start("elixir-tools.nvim report")
 
   local version_string = vim.fn.system("elixir --version")
   local ex_version = V.elixir_version(version_string)
   local otp_version = V.erlang_version(version_string)
 
   if type(ex_version) == "string" then
-    vim.health.ok("Elixir v" .. ex_version)
+    vim.health.report_ok("Elixir v" .. ex_version)
   else
-    vim.health.error("couldn't figure out elixir version")
+    vim.health.report_error("couldn't figure out elixir version")
   end
 
   if type(otp_version) == "string" then
-    vim.health.ok("OTP " .. otp_version)
+    vim.health.report_ok("OTP " .. otp_version)
   else
-    vim.health.error("couldn't figure out OTP version")
+    vim.health.report_error("couldn't figure out OTP version")
   end
 end
 
